09dab22b32f79f1e52807429f9d87ca427677975
Douglas Bullard <dbullard@nurflugel.com>
2020-11-28 12:47:46 -0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,85 @@
+group = "com.nurflugel"
+version = "0.0.1-SNAPSHOT"
+
+plugins {
+    `kotlin-dsl`
+    `java-gradle-plugin`
+    kotlin("jvm") version "1.3.72"
+    //    kotlin("jvm") version "1.4.20"
+    id("at.phatbl.shellexec") version "1.5.2"
+    id("com.github.ben-manes.versions") version "0.36.0"
+    id("com.dorongold.task-tree") version "1.5"
+    //classpath "com.gradle.publish:plugin-publish-plugin:0.10.0"
+    id("com.gradle.plugin-publish") version "0.12.0"
+}
+
+gradlePlugin {
+    plugins {
+        create("reactLambdas") {
+            id = "com.nurflugel.gradle.plugins.reactlambdas"
+            implementationClass = "com.nurflugel.gradle.plugins.reactlambdas.ReactLambdasPlugin"
+            displayName = "Gradle React-Lambdas plugin"
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+    gradlePluginPortal()
+    // your corporate plugin repo here
+}
+
+val taskTreeVersion = "1.5"
+val versionsVersion = "0.36.0"
+val shellExecVersion = "1.5.2"
+
+dependencies {
+    implementation("com.github.ben-manes:gradle-versions-plugin:$versionsVersion")
+    implementation("gradle.plugin.com.dorongold.plugins:task-tree:$taskTreeVersion")
+    implementation("gradle.plugin.at.phatbl:shellexec:$shellExecVersion")
+}
+
+//pluginBundle {
+//  // These settings are set for the whole plugin bundle
+//  website = "http://www.gradle.org/"
+//  vcsUrl = "https://github.com/gradle/gradle"
+//
+//  // tags and description can be set for the whole bundle here, but can also
+//  // be set / overridden in the config for specific plugins
+//  description = "Greetings from here!"
+//
+//  // The plugins block can contain multiple plugin entries.
+//  //
+//  // The name for each plugin block below (greetingsPlugin, goodbyePlugin)
+//  // does not affect the plugin configuration, but they need to be unique
+//  // for each plugin.
+//
+//  // Plugin config blocks can set the id, displayName, version, description
+//  // and tags for each plugin.
+//
+//  // id and displayName are mandatory.
+//  // If no version is set, the project version will be used.
+//  // If no tags or description are set, the tags or description from the
+//  // pluginBundle block will be used, but they must be set in one of the
+//  // two places.
+//
+//  (plugins) {
+//
+//    // first plugin
+//    "greetingsPlugin" {
+//      // id is captured from java-gradle-plugin configuration
+//      displayName = "Gradle Greeting plugin"
+//      tags = listOf("individual", "tags", "per", "plugin")
+//      version = "1.2"
+//    }
+//
+//    // another plugin
+//    "goodbyePlugin" {
+//      // id is captured from java-gradle-plugin configuration
+//      displayName = "Gradle Goodbye plugin"
+//      description = "Override description for this plugin"
+//      tags = listOf("different", "for", "this", "one")
+//      version = "1.3"
+//    }
+//  }
---
+++ src/main/kotlin/com/nurflugel/gradle/plugins/reactlambdas/ReactLambdasPlugin.kt
@@ -0,0 +1,311 @@
+package com.nurflugel.gradle.plugins.reactlambdas
+
+import at.phatbl.shellexec.ShellExec
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.Delete
+import org.gradle.api.tasks.Exec
+import org.gradle.internal.jvm.Jvm
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.register
+import java.io.File
+
+
+/** This class adds the tasks to get certs for affected services and install them into the current JVM */
+@Suppress("unused", "ConvertCallChainIntoSequence", "RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class ReactLambdasPlugin : Plugin<Project> {
+
+    private fun packageJsonExists(project: Project): Boolean = File(project.projectDir, "package.json").exists()
+    private fun isLambda(project: Project): Boolean = project.projectDir.toString().contains("src/lambda")
+    private fun isReact(project: Project): Boolean = project.projectDir.toString().contains("react")
+    private val reactEnvironment = System.getProperty("REACT_APP_ENVIRONMENT") ?: "local"
+
+    override fun apply(project: Project) {
+        // register plugin extension
+        val ext = project.extensions.create("reactLambdas", ReactLambdasPluginExtension::class, project)
+        applyPlugins(project)
+        registerTasks(project, ext)
+    }
+
+    private fun printBanner(project: Project, ext: ReactLambdasPluginExtension) {
+        val reactEnvironment = System.getProperty("REACT_APP_ENVIRONMENT") ?: "local"
+        val prefix = """
+            ========================================================================================================
+            Welcome to Gradle version:          ${project.gradle.gradleVersion}
+            Java version:                       ${Jvm.current()}
+            Java home:                          ${Jvm.current().javaHome}
+            Gradle user directory is set to:    ${project.gradle.gradleUserHomeDir}
+            Project directory:                  ${project.projectDir}
+            Running build script:               ${project.buildFile}
+            ReactEnvironment:                   $reactEnvironment
+            Subprojects:                        ${project.subprojects.map { it.name }}
+            """.trimIndent()
+        println(prefix)
+    }
+
+    /** Apply the specified plugins to the project */
+    private fun applyPlugins(project: Project) = with(project) {
+        pluginManager.apply("idea")
+        pluginManager.apply("project-report")
+        pluginManager.apply("com.github.ben-manes.versions")
+        pluginManager.apply("com.dorongold.task-tree")
+        pluginManager.apply("at.phatbl.shellexec")
+    }
+
+    /** Here's where we specify the tasks for the project and/or subprojects */
+    private fun registerTasks(project: Project, ext: ReactLambdasPluginExtension) = with(project) {
+
+        /** Everything in subprojects applies only to the sub-projects - not the global project */
+        subprojects {
+            tasks {
+                tasks.register<ShellExec>("install") {
+                    description = "Do the 'install' operation"
+                    group = "DRA"
+                    // this looks pretty fancy - but this is how we can have overridable commands per-project.  You could override it by having
+                    // this bit in your project's build.gradle:
+                    // ccReactLambdas {
+                    //    installLambdaCommand = "npm install --verbose"
+                    // }
+                    command = when {
+                        isReact(this.project) -> ext.installReactCommand ?: "npm install"
+                        else                  -> ext.installLambdaCommand ?: "yarn"
+                    }
+                    onlyIf { packageJsonExists(this.project) } // using this.project gets the current subproject - not the global project
+
+                }
+                tasks.register<ShellExec>("installProduction") {
+                    description = "Package the production dependencies.  Run it like this: 'gradle installProduction -DREACT_APP_ENVIRONMENT=dev"
+                    group = "DRA"
+                    command = when {
+                        isReact(this.project) -> ext.installReactProductionCommand ?: "npm run build"
+                        else                  -> ext.installLambdaProductionCommand ?: "yarn --production"
+                    }
+                    onlyIf { packageJsonExists(this.project) }
+                    doLast { println("The env used is: $reactEnvironment") }
+                }
+                tasks.register<ShellExec>("test") {
+                    description = "Run the tests for both React and Lambdas"
+                    group = "DRA"
+                    command = when {
+                        isReact(this.project) -> ext.testReactCommand ?: "npm run test-coverage"
+                        else                  -> ext.testLambdaCommand ?: "yarn test --testTimeout=10000"
+                    }
+                    onlyIf { packageJsonExists(this.project) }
+                }
+
+                tasks.register<ShellExec>("lint") {
+                    description = "Lint the code"
+                    group = "DRA"
+                    command = when {
+                        isReact(this.project) -> ext.lintReactCommand ?: "npm run lint-ts"
+                        else                  -> ext.lintLambdaCommand ?: "yarn run lint"
+                    }
+                    onlyIf { packageJsonExists(this.project) && (file(".eslintrc").exists() || file(".eslintrc.js").exists()) }
+                }
+
+                tasks.register<ShellExec>("tsc") {
+                    description = "Run tsc on any project that has a tsconfig.json "
+                    group = "DRA"
+                    command = "yarn run tsc"
+                    onlyIf {
+                        val lambda = isLambda(this.project)
+                        val exists = file("tsconfig.json").exists()
+                        println("tsconfig.json exists = ${exists}")
+                        lambda && exists
+                    }
+                }
+
+
+                // React only
+                tasks.register<ShellExec>("runReact") {
+                    description = "Run the React app like this: 'gradle runReact -DREACT_APP_ENVIRONMENT=dev'.  If -DREACT_APP_ENVIRONMENT is omitted, local is used"
+                    group = "DRA"
+                    command = ext.runReactCommand ?: "npm run start-$reactEnvironment"
+                    onlyIf { isReact(this.project) }
+                }
+
+
+                // React only
+                tasks.register<ShellExec>("buildCss") {
+                    description = "Build the CSS"
+                    group = "DRA"
+                    command = ext.buildCssCommand ?: "npm run build-css"
+                    onlyIf { isReact(this.project) }
+                }
+
+                tasks.register<Exec>("cleanUpForDeploy") {
+                    description = "Remove any files in the lamgda dirs not needed for deployment - this WILL delete source-controlled files, so run locally with care!"
+                    group = "DRA"
+                    isIgnoreExitValue = true
+                    commandLine = "ls -l serverless.yaml event.json package-lock.json package.json serverless.yaml jest-config.js test *.txt *.ts yarn.lock jest.config.js".split(
+                            " "
+                                                                                                                                                                                 )
+                    onlyIf { isLambda(this.project) && packageJsonExists(this.project) }
+                }
+
+                // below are housekeeping tasks
+                tasks.register<ShellExec>("createModulesList") {
+                    description = "create a list of the contents for each module and export that into a file - very useful for fast comparisons of different builds"
+                    group = "DRA Housekeeping"
+                    command = "ls -w1 node_modules > node_modules.out"
+                    onlyIf { packageJsonExists(this.project) }
+                }
+
+                tasks.register<Delete>("cleanNodeModules") {
+                    description = "Clean the node modules dirs"
+                    group = "DRA Housekeeping"
+                    delete = setOf("node_modules", "node_modules.out")
+                }
+            }
+        }
+        tasks.register<ShellExec>("mergeTestResults") {
+            description = "Merge all the test results"
+            group = "DRA"
+            command = """
+        mkdir -p ../lcov
+        """ + createMergeCommands(project)
+        }
+
+        tasks.register<ShellExec>("cleanCoverage") {
+            description = "Clean out the coverage folders between tests"
+            group = "DRA Housekeeping"
+            command = "find . -name coverage | grep -v modules | xargs rm -rf"
+        }
+        tasks.register("regenerateSettingsDotGradle") {
+
+            description = "Utility class to regenerate the tools/settings.gradle.kts file - if you create more lambdas, run this "
+            group = "DRA Housekeeping"
+            doLast {
+                // create the file header
+                val stringBuilder = StringBuilder("""rootProject.name = "${rootProject.name}"
+"""
+                                                 )
+
+                // deal with the react contents if they exist
+                val reactDir = File("react")
+                println("React dir exists: ${reactDir.exists()}")
+                if (reactDir.exists()) {
+                    stringBuilder.append("""
+
+include("react")
+project(":react").projectDir = File("../react")
+"""
+                                        )
+                }
+
+                // Now create a mapping entry for each lambda
+                val lambdasDir = File("src/lambda")
+                if (lambdasDir.exists()) {
+                    lambdasDir.listFiles()
+                            .filter {
+                                val hasPackageJson = it.listFiles()?.map { file -> file.name }?.any { name -> name == "package.json" }
+                                                     ?: false
+                                hasPackageJson
+                            }
+                            .map { it.name }
+                            .sorted()
+                            .map {
+                                """
+include("$it")
+project(":$it").projectDir = File("../src/lambda/$it")
+"""
+                            }
+                            .forEach { stringBuilder.append(it) }
+
+                }
+
+                // finally, add the plugin repo settings
+                stringBuilder.append("""
+
+            pluginManagement {
+                repositories {
+                    gradlePluginPortal()
+                    mavenLocal()
+                }
+            } 
+        """.trimIndent()
+                                    )
+
+                // write it to disk
+                val fileContents = stringBuilder.toString()
+                File("tools/settings.gradle.kts").writeText(fileContents)
+
+            }
+        }
+
+
+        tasks.register<ShellExec>("outputToolVersions") {
+            description = "Show the tool versions"
+            group = "DRA Housekeeping"
+
+            command = """
+        echo "Git version " $(git --version)
+        echo "NPM version " $(npm --version)
+        echo "Node version" $(node --version)
+        echo "Yarn version" $(yarn --version)
+        echo "Tsc version " $(tsc --version)
+        echo "pip3 version " $(pip3 -V)
+        echo "junit-merge version " $(junit-merge -V)
+        ${ext.outputToolVersionsExtra ?: ""}
+        """ + createMergeCommands(project)
+        }
+
+        afterEvaluate {
+            // we print the banner here as it needs to be done AFTER evaluation so the vars can be read in
+            printBanner(project, ext)
+        }
+    }
+
+    /** Some of the tools don't give you an option to ignore stuff that's not there and fail, so we have to have different
+     * commands for whether or not React is there.  It's assumed that there is at least one lambda in the project; else, we'll
+     * have to have a third condition of react w/no lambdas.
+     */
+    private fun createMergeCommands(project: Project): String {
+        val hasReact = File("${project.projectDir}/..")
+                .listFiles()
+                .any { it.name == "react" }
+
+        println("hasReact files is $hasReact")
+        return when {
+            hasReact -> {
+                """
+            #  First, copy results from the React dir into the lcov dir - OK if it's not there
+    lcov-result-merger '../react/coverage/lcov.info' '../lcov/react_lcov.info' || true
+            #  Next, merge any lambdas into the lcov dir
+    lcov-result-merger '../src/lambda/*/coverage/lcov.info' '../lcov/lambdas_lcov.info'
+            #  Finally, merge it all together so we have SINGLE file we can send to QMA
+    lcov-result-merger '../lcov/*_lcov.info' '../lcov/results-lcov.info' || true
+
+            # Next, run the command to merge all the Junit xml files into one toplevel xml file - https://www.npmjs.com/package/junit-merge
+    junit-merge  ../src/lambda/*/coverage/jest/junit.xml -o ../allLambdasMergedTests.xml || true
+    junit-merge  ../react/coverage/jest/junit.xml  -o ../allReactMergedTests.xml || true
+    junit-merge  ../allLambdasMergedTests.xml ../allReactMergedTests.xml -o ../allMergedTests.xml || true
+
+     #  Finally, create an HTML report from THAT via junit2html - https://github.com/inorton/junit2html
+     junit2html ../allMergedTests.xml ../allMergedTests.html
+"""
+            }
+            else     -> { // React is missing, so these commands need to be adjusted
+                """
+            #  Merge any lambdas into the lcov dir
+    lcov-result-merger '../src/lambda/*/coverage/lcov.info' '../lcov/lambdas_lcov.info'
+            #  Finally, merge it all together so we have SINGLE file we can send to QMA
+    lcov-result-merger '../lcov/*_lcov.info' '../lcov/results-lcov.info' || true
+
+            # Next, run the command to merge all the Junit xml files into one toplevel xml file - https://www.npmjs.com/package/junit-merge
+
+    junit-merge  ../src/lambda/*/coverage/jest/junit.xml -o ../allLambdasMergedTests.xml || true
+    junit-merge  ../allLambdasMergedTests.xml -o ../allMergedTests.xml || true
+
+     #  Finally, create an HTML report from THAT via junit2html - https://github.com/inorton/junit2html
+     junit2html ../allMergedTests.xml ../allMergedTests.html
+"""
+            }
+        }
+    }
+}
+
+
+
+
